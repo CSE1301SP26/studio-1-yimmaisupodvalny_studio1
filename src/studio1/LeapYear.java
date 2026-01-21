34fc4e4bbb7a957c5be0a2b81ef7b4471811096c
Mywustl18 <mai.n@wustl.edu>
2026-01-21 15:30:01 -0600

    LeapYear Done

--- src/studio1/LeapYear.java
+++ src/studio1/LeapYear.java
@@ -7,7 +7,9 @@ public class LeapYear {
 public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
+System.out.println("Enter a year");
 int Year = in.nextInt();
-boolean isLeapYear =
+boolean isLeapYear = (Year % 4 ==0) && (Year % 100 != 0) || (Year % 400 == 0);
+System.out.println(isLeapYear);
 }
 }
